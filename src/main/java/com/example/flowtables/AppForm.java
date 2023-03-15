9d38f8e74743c85eb335f8ca7d7d5bb6762e6b19
Paval-from-Belarus <star-war-sandakan@mail.ru>
2023-03-16 00:29:13 +0300

    update token list

--- src/main/java/com/example/flowtables/AppForm.java
+++ src/main/java/com/example/flowtables/AppForm.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 
 public class AppForm extends Application {
@@ -14,17 +12,7 @@ public class AppForm extends Application {
 public void start(Stage stage) throws IOException {
       FXMLLoader fxmlLoader = new FXMLLoader(AppForm.class.getResource("flow-tables.fxml"));
       Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-      TableBuilder builder = new TableBuilder();
-      try(BufferedReader reader = new BufferedReader(new FileReader("source.pl"))){
-	    String line;
-	    StringBuilder buffer = new StringBuilder();
-	    while((line = reader.readLine()) != null){
-		  buffer.append(line).append("\n");
-	    }
-	    builder.setSource(buffer.toString());
-      }
       FormController.stage = stage;
-      builder.start();
       stage.setTitle("FlowTables 3100");
       stage.setScene(scene);
       stage.show();
