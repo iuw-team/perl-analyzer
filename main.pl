2ed147fbb2eec0c602ed5eea4cee21173546de4a
Paval-from-Belarus <star-war-sandakan@mail.ru>
2023-03-16 23:42:19 +0300

    update test file

--- main.pl
+++ main.pl
@@ -4,11 +4,11 @@ use warnings;
 my %table = (key_0 => 'value_0',
              key_1 => 'value_1',
              key_2 => (1, 3, 5));
-foreach my $value (%table){
+foreach (%table){
     for(my $i = 1; $i < 10; $i += 1){
         for(my $j = 1; $j < 100; $j += 3){
             my $state = 0;
-            my $temp = 0;
+            my $temp;
             for(my $k = 2; $k < 1000 && $state != 1; $k += 1){
                 $temp = hardCalc($i, $j, $k);
                 if($temp > 100){
@@ -16,32 +16,16 @@ foreach my $value (%table){
                 }
                 elsif ($temp > 50){
                     print "Almost'";
-                }
-                elsif ($temp > 10){
-                    print "Very far";
-                }
-                elsif ($temp < 2){
-                    print "Too small!";
+                    if($temp > 55){
+                        print "Almost-Almost\n"; }
+                    if($temp < 55){
+                        print "Not so Almost"; }
+                    else {print "Enough";}
                 }
                 else {
                     print "Not now";
                 }
             }
         }
-    }
-}
-my ($x, $y, $z) = (0, 12, 55);
-if (($x < 11) || ($y > 11)){
-    if($y < 11 && $z > 43){
-        print"Let's me do some magic";
-    }
-    elsif(($x > 11) && ($z > 12)){
-        print "Another text";
-    }
-    elsif(getHugeNumber() > 11) {
-        print "Are you crazy?)";
-    }
-    else {
-        print "Finally!";
     }
 }
